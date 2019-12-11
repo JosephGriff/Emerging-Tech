2f5dcc24abc61fef1d733443a4b57d94f0bf88a2
JosephGriff <34632266+JosephGriff@users.noreply.github.com>
2019-12-11 17:59:16 +0000

    Webapp started

---
+++ WebApp/web-app.py
@@ -0,0 +1,11 @@
+## Author = Joseph  Griffith
+## Student No. = G00350112
+
+## Web Application Using Flask
+
+## Imports added so far 
+import flask as fl
+from keras.models import load_model
+
+## Load Model Created in Jupyter Notebook
+model = load_model('Digit_Predicter.h5')
